1462021a565eb91ffee859e0354550ba249a2fb3
shantanu1971 <sbtalk@gmail.com>
2017-10-10 09:59:36 +0530

    examples

---
+++ workspace2-ibm-pune/emp-rest-service/src/main/java/com/demo/spring/EmpApp.java
@@ -0,0 +1,14 @@
+package com.demo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpApp.class, args);
+
+	}
+
+}
---
+++ workspace2-ibm-pune/emp-rest-service/src/main/java/com/demo/spring/EmpController.java
@@ -0,0 +1,39 @@
+package com.demo.spring;
+
+import java.util.HashMap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.spring.model.Emp;
+
+@RestController
+public class EmpController {
+	
+	static HashMap<Integer, Emp> empDb=new HashMap<>();
+	static{
+		empDb.put(100, new Emp(100,"Kabir","Hyderabad",34000));
+		empDb.put(101, new Emp(101,"James","Bangalore",56000));
+		empDb.put(102, new Emp(102,"Scott","Hyderabad",54000));
+		empDb.put(103, new Emp(103,"Kirti","Pune",34000));
+		empDb.put(104, new Emp(104,"Ketan","Hyderabad",45000));
+	}
+
+	@RequestMapping(path="/find", method=RequestMethod.GET,produces="application/json")
+	//http://host:port/find?empid=101
+	public ResponseEntity getEmpById(@RequestParam("empid")int id){
+		if(empDb.containsKey(id)){
+			return ResponseEntity.ok(empDb.get(id));
+		}else{
+			return ResponseEntity.ok("Employee Not Found");
+		}
+	}
+	
+	@RequestMapping(path="/info", method=RequestMethod.GET)
+	public String getInfo(){
+		return "This is a demo REST Service in Spring";
+	}
+}
